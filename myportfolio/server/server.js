a33147e4c30becadaf33015c3d4806a5ef6e67a7
Mugdha1502 <Mugdha1502>
2024-06-25 15:18:26 +0530

    my first commit

--- myportfolio/server/server.js
+++ myportfolio/server/server.js
@@ -3,21 +3,21 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 dotenv.config();
 
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
-console.log('PORT:', process.env.PORT);
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -35,12 +35,30 @@ const contactSchema = new mongoose.Schema({
 
 const Contact = mongoose.model('Contact', contactSchema);
 
+// Function to send Telegram notification
+const sendTelegramNotification = async (message) => {
+  const url = `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/sendMessage`;
+  try {
+    await axios.post(url, {
+      chat_id: TELEGRAM_CHAT_ID,
+      text: message,
+    });
+  } catch (error) {
+    console.error('Error sending Telegram message:', error);
+  }
+};
+
 // Routes
 app.post('/api/contact', async (req, res) => {
   try {
     const { name, email, phone, message } = req.body;
     const newContact = new Contact({ name, email, phone, message });
     await newContact.save();
+
+    // Send Telegram notification
+    const notificationMessage =` New Contact Submission:\n\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`;
+    await sendTelegramNotification(notificationMessage);
+
     res.status(201).send('Message sent successfully!');
   } catch (error) {
     console.error('Error saving contact message:', error);
