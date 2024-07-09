855627e023b18134dce45ddfbfce89347294356a
Mugdha1502 <Mugdha1502>
2024-07-09 18:18:06 +0530

    my first commit

--- myportfolio/src/components/Experience/Experience.jsx
+++ myportfolio/src/components/Experience/Experience.jsx
@@ -6,25 +6,26 @@ import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
   return (
-    <section className={styles.container} id="experience">
-      <h2 className={styles.title}>Experience</h2>
-      <div className={styles.content}>
-        <div className={styles.skills}>
-          {skills.map((skill, id) => {
-            return (
-              <div key={id} className={styles.skill}>
-                <div className={styles.skillImageContainer}>
-                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+    <>
+      <section className={styles.container} id="experience">
+        <h2 className={styles.title}>Experience</h2>
+        <div className={styles.content}>
+          <div className={styles.skills}>
+            {skills.map((skill, id) => {
+              return (
+                <div key={id} className={styles.skill}>
+                  <div className={styles.skillImageContainer}>
+                    <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                  </div>
+                  <p>{skill.title}</p>
                 </div>
-                <p>{skill.title}</p>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
-
 
 export default Experience;
